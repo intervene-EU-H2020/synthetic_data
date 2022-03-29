a249d2d7c7134ace0534e1b56aecb4d3205e743e
Vishnu Raj <vishnu.raj@aalto.fi>
2022-03-29 12:56:57 +0300

    PCA eval fix

--- evaluation/metrics/eval_pca.jl
+++ evaluation/metrics/eval_pca.jl
@@ -37,8 +37,19 @@ function pca_alignment(real_data_prefix, synt_data_prefix)
     var_real = pc_real_eigval ./ sum(pc_real_eigval)
     var_synt = pc_synt_eigval ./ sum(pc_synt_eigval)
 
-    ## Compute how aligned each of the PCs are -- ideally this should be close to 1
-    align_val = diag(pc_real_norm' * pc_synt_norm)
+    ## Iteratively find out which PC in synthetic data is closest match to real data
+    n_pcs = size(pc_real_norm, 2) # number of PCs
+    idx = collect(1:n_pcs) # Indices of synthetic PCs to check
+    align_val = []
+    for i ∈ 1:n_pcs
+        # For i-th real PC, find the closest match
+        _pcs = pc_synt_norm[:, idx]
+        _align_vals = pc_real_norm[:, i]' * _pcs
+        _align_vals = vec(abs.(_align_vals)) # Direction may be different
+        v, j = findmax(_align_vals) # find the best pc align value and it's index
+        deleteat!(idx, j) # remove this pc index from list
+        append!(align_val, v) # add this pc alignment to the align value
+    end
 
     # Compute weighted alignment according to variance captured -- ideally close to 1
     wgt_align = sum(var_real .* align_val)
