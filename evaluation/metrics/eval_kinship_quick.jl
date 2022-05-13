73be4fb869d26efa75e6c18fae586f196c711865
sophiewharrie <sophie_wharrie@outlook.com>
2022-05-13 14:13:10 +0300

    kinship update

--- evaluation/metrics/eval_kinship_quick.jl
+++ evaluation/metrics/eval_kinship_quick.jl
@@ -6,7 +6,7 @@ function kinship_cross(synfile, nsamples_synfile, reffile, king, out_prefix)
     input = @sprintf("%s.bed", synfile)
     cross_input = @sprintf("%s.bed", reffile)
     
-    run(`$king -b $input,$cross_input --related --degree 2 --prefix $output`)
+    run(`$king -b $input,$cross_input --related --prefix $output`)
 
     kinship_file = @sprintf("%s.kin0", output)
     if isfile(kinship_file)
@@ -20,16 +20,14 @@ function kinship_cross(synfile, nsamples_synfile, reffile, king, out_prefix)
         
         duplicate = kin_cross_filtered[kin_cross_filtered.Kinship .> 0.354, :]
         first_degree = kin_cross_filtered[0.177 .< kin_cross_filtered.Kinship .<= 0.354, :]
-        second_degree = kin_cross_filtered[0.0884 .< kin_cross_filtered.Kinship .<= 0.177, :]
     end
         
     # compute proportion of samples in synthetic data that are close relatives of reference data
     duplicate_samples = isfile(kinship_file) ? length(Set(duplicate.ID1))/nsamples_synfile : 0
     first_degree_samples = isfile(kinship_file) ? length(Set(first_degree.ID1))/nsamples_synfile : 0
-    second_degree_samples = isfile(kinship_file) ? length(Set(second_degree.ID1))/nsamples_synfile : 0
-    total_related_samples = duplicate_samples + first_degree_samples + second_degree_samples
+    total_related_samples = duplicate_samples + first_degree_samples
     
-    data = [duplicate_samples, first_degree_samples, second_degree_samples, total_related_samples]
+    data = [duplicate_samples, first_degree_samples, total_related_samples]
     
     return [1:length(data) data]
 end
@@ -39,23 +37,25 @@ function kinship_syn(synfile, nsamples_synfile, king, out_prefix)
     output = @sprintf("%s.syn.king", out_prefix)
     input = @sprintf("%s.bed", synfile)
 
-    run(`$king -b $input --related --degree 2 --prefix $output`)
+    run(`$king -b $input --related --prefix $output`)
 
     kinship_file = @sprintf("%s.kin0", output)
     if isfile(kinship_file)
         kin_data = CSV.File(kinship_file, delim='\t') |> DataFrame 
         duplicate = kin_data[kin_data.Kinship .> 0.354, :]
         first_degree = kin_data[0.177 .< kin_data.Kinship .<= 0.354, :]
-        second_degree = kin_data[0.0884 .< kin_data.Kinship .<= 0.177, :]
     end
     
     # compute number of related pairs in synthetic samples
-    duplicate_samples = isfile(kinship_file) ? length(Set(duplicate.ID1)) : 0
-    first_degree_samples = isfile(kinship_file) ? length(Set(first_degree.ID1)) : 0
-    second_degree_samples = isfile(kinship_file) ? length(Set(second_degree.ID1)) : 0
-    total_related_samples = duplicate_samples + first_degree_samples + second_degree_samples
+    duplicate_pairs = isfile(kinship_file) ? nrow(duplicate) : 0
+    first_degree_pairs = isfile(kinship_file) ? nrow(first_degree) : 0
+    total_related_pairs = duplicate_pairs + first_degree_pairs
+
+    duplicate_samples = isfile(kinship_file) ? length(Set(hcat(duplicate.ID1, duplicate.ID2))) : 0
+    first_degree_samples = isfile(kinship_file) ? length(Set(hcat(first_degree.ID1, first_degree.ID2))) : 0
+    total_related_samples = duplicate_samples + first_degree_samples
     
-    data = [duplicate_samples, first_degree_samples, second_degree_samples, total_related_samples]
+    data = [duplicate_pairs, first_degree_pairs, total_related_pairs, duplicate_samples, first_degree_samples, total_related_samples]
     
     return [1:length(data) data]
 end
@@ -63,10 +63,7 @@ end
 
 function kinship_quick(synfile, nsamples_synfile, reffile, king, out_prefix)
     syn = kinship_syn(synfile, nsamples_synfile, king, out_prefix)
-    #cross = kinship_cross(synfile, nsamples_synfile, reffile, king, out_prefix)
-    
-    #df = DataFrame(metric=["syn_duplicates", "syn_1st_degree", "syn_2nd_degree", "syn_combined", "cross_duplicates", "cross_1st_degree", "cross_2nd_degree", "cross_combined"], value=[syn[1,2], syn[2,2], syn[3,2], syn[4,2], cross[1,2], cross[2,2], cross[3,2], cross[4,2]])
-    df = DataFrame(metric=["syn_duplicates", "syn_1st_degree", "syn_2nd_degree", "syn_combined"], value=[syn[1,2], syn[2,2], syn[3,2], syn[4,2]])
+    df = DataFrame(metric=["syn_duplicate_pairs", "syn_1st_degree_pairs", "syn_combined_pairs", "syn_duplicate_samples", "syn_1st_degree_samples", "syn_combined_samples"], value=[syn[1,2], syn[2,2], syn[3,2], syn[4,2], syn[5,2], syn[6,2]])
     outfile = joinpath(dirname(out_prefix), "results-relatedness.csv")
     CSV.write(outfile, df)
     @info "Relatedness data saved at $outfile"
